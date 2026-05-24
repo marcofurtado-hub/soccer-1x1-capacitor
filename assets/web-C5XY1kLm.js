2bf0d113aed540a5b5f8e1639725a1f8067cd787
Marco Furtado <marco.furtado@wildlifestudios.com>
2026-05-24 19:01:26 -0300

    Add surrender button next to match timer

--- assets/web-C5XY1kLm.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./index-DtZ2GwS6.js";class t extends e{getStatus(){return Promise.resolve({status:"authorized"})}requestPermission(){return Promise.resolve({status:"authorized"})}}export{t as AppTrackingTransparencyWeb};
---
+++ assets/web-D6cMbpKF.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-ddhmhB_e.js";class t extends e{getStatus(){return Promise.resolve({status:"authorized"})}requestPermission(){return Promise.resolve({status:"authorized"})}}export{t as AppTrackingTransparencyWeb};
