f483d406693b5f279cfde16450079fb1e3211034
Marco Furtado <marco.furtado@wildlifestudios.com>
2026-06-14 00:52:31 -0300

    v1.1.2: smarter+harder bot + curved-swipe banana shots

---
+++ assets/web-BpIXxXG7.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-CX056T0l.js";class t extends e{getStatus(){return Promise.resolve({status:"authorized"})}requestPermission(){return Promise.resolve({status:"authorized"})}}export{t as AppTrackingTransparencyWeb};
