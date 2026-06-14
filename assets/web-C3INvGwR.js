2c84d809c4912e082482a839a376ca71c36e9c67
Marco Furtado <marco.furtado@wildlifestudios.com>
2026-06-14 00:14:45 -0300

    v1.1.1: revert island art back to v1.0.9 look

--- assets/web-C3INvGwR.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./index-D_BRjhU8.js";class t extends e{getStatus(){return Promise.resolve({status:"authorized"})}requestPermission(){return Promise.resolve({status:"authorized"})}}export{t as AppTrackingTransparencyWeb};
---
+++ assets/web-CjQ-HAKw.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-BJV0yoWZ.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
